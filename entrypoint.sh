0af44a7edcedb09fc4c95bf14498b81fc8fbd32a
gonvilch@ucm.es <gonvilch@ucm.es>
2024-08-14 18:05:09 +0200

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@
 /app/.venv/bin/python manage.py migrate
 
 # Iniciar Gunicorn
-exec gunicorn -w 3 WebMaude.wsgi:application -b 0.0.0.0:8000
+/app/.venv/bin/python -m gunicorn -w 3 WebMaude.wsgi:application -b 0.0.0.0:8000
