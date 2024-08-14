272dbdc6a97be6df85bce8418cfc587eba611999
gonvilch@ucm.es <gonvilch@ucm.es>
2024-08-14 20:43:00 +0200

    cambio entrypoint root

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,8 @@
 # Ejecutar migraciones
 /app/.venv/bin/python manage.py migrate
 
+export DJANGO_SUPERUSER_PASSWORD='root'
+/app/.venv/bin/python manage.py createsuperuser --noinput --username root --email ''
+
 # Iniciar Gunicorn
 /app/.venv/bin/python -m gunicorn -w 3 WebMaude.wsgi:application -b 0.0.0.0:8000
