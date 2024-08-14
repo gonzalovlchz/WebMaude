6aec75c46b5c39a20d0998125c9999459522222e
gonzalovlchz <100685435+gonzalovlchz@users.noreply.github.com>
2024-08-14 17:48:11 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Ejecutar migraciones
-python manage.py migrate
+/app/.venv/bin/python manage.py migrate
 
 # Iniciar Gunicorn
 exec gunicorn -w 3 WebMaude.wsgi:application -b 0.0.0.0:8000
